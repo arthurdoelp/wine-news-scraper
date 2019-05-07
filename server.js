a6c7aa0788d8dc6e8472926849e74810fbc80d0c
arthurdoelp <arthurdoelp@gmail.com>
2019-05-07 12:58:00 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
 
 mongoose.connect(MONGODB_URI);
 
-var PORT = proces.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
